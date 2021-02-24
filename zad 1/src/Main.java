7a36622bb04435acd05a764a87f8078ab157ec4c
MateuszFrąckiewicz <79456002+MateuszF123@users.noreply.github.com>
2021-02-24 16:33:08 +0100

    za

--- zad 1/src/Main.java
+++ zad 1/src/Main.java
@@ -1,18 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        //zadanie 1
         int A=5;
         double B=4.2;
         char C='@';
         String D="to";
         System.out.println(A+" "+B+" "+C +" " +D);
-
+    //zadanie 2
         int a=1,b=2;
         System.out.println("wynik = "+ a*a+b*b);
-
+    //zadanie 3
         System.out.println("pole wynosi "+(7*7)/2);
         double sqrt=Math.sqrt(9);
         System.out.println(sqrt);
-
+    //zadanie 4
         String i="Mateusz";
         String n="Frąckiewicz";
         System.out.println(i);
