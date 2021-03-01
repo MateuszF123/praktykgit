612a186b040b35a7e5d766e48cb5799eb4988ed7
MateuszFrąckiewicz <79456002+MateuszF123@users.noreply.github.com>
2021-03-01 13:23:38 +0100

    zad1-4

---
+++ zadania/src/klasy/zad4/Main_okrąg.java
@@ -0,0 +1,16 @@
+package klasy.zad4;
+
+
+
+public class Main_okrąg {
+    public static void main(String[] args) {
+        okrąg okrąg=new okrąg(5);
+        okrąg.setŚrednica();
+        okrąg.setPole();
+        okrąg.obwk();
+    }
+
+
+
+
+}
